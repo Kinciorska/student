02a77636170769ffc3f4b498a8963ccd9d89ff90
Kinciorska <kinga.kincso.kollo@gmail.com>
2024-01-04 09:43:26 +0100

    feature: frontend - added NewStudent modal

--- students-front/src/components/Header.js
+++ students-front/src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
     return (
       <div className="text-center">
         <img
-          src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.saaspegasus.com%2Fguides%2Fmodern-javascript-for-django-developers%2Fintegrating-django-react%2F&psig=AOvVaw3iKYOlmP9jG639olK13sx2&ust=1703238230000000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNiCyP-eoIMDFQAAAAAdAAAAABAD"
+          src="https://www.saaspegasus.com/static/images/web/modern-javascript/django-react-header.51a983c82dcb.png"
           width="300"
           className="img-thumbnail"
           style={{ marginTop: "20px" }}
