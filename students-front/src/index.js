966aa47319934dda20267e6a9ea447e84db925e3
Kinciorska <kinga.kincso.kollo@gmail.com>
2024-01-05 09:13:48 +0100

    feature: frontend - added StudentList component

--- students-front/src/index.js
+++ students-front/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Header from './components/Header.js';
+import StudentList from './components/StudentList.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    <StudentList />
   </React.StrictMode>
 );
 
